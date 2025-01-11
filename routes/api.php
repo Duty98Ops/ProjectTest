b0870bfd0f75d692449434fe775fb22a52b1187f
Bagus Setiawan <71681424+Duty98Ops@users.noreply.github.com>
2025-01-11 17:27:50 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,7 @@
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-// Grup rute yang memerlukan autentikasi JWT
+
 Route::middleware('auth:api')->group(function () {
-    // Menggunakan API resource untuk TaskController
     Route::apiResource('tasks', TaskController::class);
 });
